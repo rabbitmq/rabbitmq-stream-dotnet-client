9480181f4e2550f00d34b08bfb9e9c3ec7a33f25
Gabriele Santomaggio <G.santomaggio@gmail.com>
2021-12-07 18:08:14 +0100

    Add more execption

--- RabbitMQ.Stream.Client/ClientExceptions.cs
+++ RabbitMQ.Stream.Client/ClientExceptions.cs
@@ -4,35 +4,42 @@ namespace RabbitMQ.Stream.Client
 {
     internal static class ClientExceptions
     {
-        
         public static void MaybeThrowException(ResponseCode responseCode, string message)
         {
             if (responseCode != ResponseCode.Ok)
             {
                 throw responseCode switch
                 {
-                    ResponseCode.VirtualHostAccessFailure => new VirtualHostAccessFailureException(message),
-                    //TODO Implement the other responses
-                    _ => new NotImplementedException()
+                    ResponseCode.VirtualHostAccessFailure => new VirtualHostAccessFailureException(responseCode,
+                        message),
+                    //TODO Implement for all the response code
+                    _ => new GenericProtocolException(responseCode, message)
                 };
             }
         }
-        
     }
-    
+
     public class ProtocolException : Exception
     {
-        protected ProtocolException(string s) : base(s)
+        protected ProtocolException(ResponseCode responseCode, string s) :
+            base($"response code: {responseCode} - {s}")
         {
+        }
+    }
 
+    public class GenericProtocolException : ProtocolException
+    {
+        public GenericProtocolException(ResponseCode responseCode, string s) :
+            base(responseCode, s)
+        {
         }
     }
 
     public class VirtualHostAccessFailureException : ProtocolException
     {
-        public VirtualHostAccessFailureException(string s) : base(s)
+        public VirtualHostAccessFailureException(ResponseCode responseCode, string s) :
+            base(responseCode, s)
         {
-
         }
     }
 }
