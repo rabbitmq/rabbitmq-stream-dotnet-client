d3860ec5b77b584c82f7a658b47f693952d52785
Gabriele Santomaggio <G.santomaggio@gmail.com>
2022-01-19 11:58:29 +0100

    Add ProducerMixingDifferentConfirmations test

--- Tests/ProducerSystemTests.cs
+++ Tests/ProducerSystemTests.cs
@@ -131,6 +131,7 @@ namespace Tests
         [WaitTestBeforeAfter]
         public async void ProducerMessagesListLenValidation()
         {
+            // by protocol the subBatchList is ushort
             var messages = new List<Message>();
             for (var i = 0; i < (ushort.MaxValue + 1); i++)
             {
@@ -158,6 +159,64 @@ namespace Tests
             }
 
             Assert.Equal(ResponseCode.Ok, await producer.Close());
+            await system.DeleteStream(stream);
+            await system.Close();
+        }
+
+
+        [Fact]
+        [WaitTestBeforeAfter]
+        public async void ProducerMixingDifferentConfirmations()
+        {
+            // we send 150 messages using three different ways:
+            // 1- 50 messages subBatch with compress None 
+            // 2- 50 messages with standard send 
+            // 3- subBatch with compress Gzip
+            // we should receive only 52 conformations
+            // one for the point 1
+            // 50 for the point 2
+            // one for the point 3
+            var messages = new List<Message>();
+            for (var i = 0; i < 50; i++)
+            {
+                messages.Add(new Message(Encoding.UTF8.GetBytes($"sub{i}")));
+            }
+
+            var testPassed = new TaskCompletionSource<bool>();
+
+            var stream = Guid.NewGuid().ToString();
+            var config = new StreamSystemConfig();
+            var system = await StreamSystem.Create(config);
+            await system.CreateStream(new StreamSpec(stream));
+            ulong count = 0;
+            var producer = await system.CreateProducer(
+                new ProducerConfig
+                {
+                    Reference = "producer",
+                    Stream = stream,
+                    ConfirmHandler = async confirmation =>
+                    {
+                        count = Interlocked.Increment(ref count);
+                        if (count == 52)
+                        {
+                            testPassed.SetResult(true);
+                        }
+                    }
+                });
+
+            ulong pid = 0;
+            await producer.Send(++pid, messages, CompressMode.None);
+
+            foreach (var message in messages)
+            {
+                await producer.Send(++pid, message);
+            }
+
+            await producer.Send(++pid, messages, CompressMode.Gzip);
+            Thread.Sleep(500);
+            Assert.Equal(ResponseCode.Ok, await producer.Close());
+            new Utils<bool>(testOutputHelper).WaitUntilTaskCompletes(testPassed);
+            Assert.Equal((ulong) 52, count);
             await system.DeleteStream(stream);
             await system.Close();
         }
