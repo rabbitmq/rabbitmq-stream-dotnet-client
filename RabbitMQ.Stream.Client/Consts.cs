b7e133c502df4cc2369d1455572a45afaf69dcf1
Gabriele Santomaggio <G.santomaggio@gmail.com>
2022-02-16 13:54:37 +0100

    update version to 1.0.0-beta.3

--- RabbitMQ.Stream.Client/Consts.cs
+++ RabbitMQ.Stream.Client/Consts.cs
@@ -2,6 +2,6 @@ namespace RabbitMQ.Stream.Client
 {
     public static class Consts
     {
-        public const string ClientVersion = "1.0.0-beta.2";
+        public const string ClientVersion = "1.0.0-beta.3";
     }
 }
