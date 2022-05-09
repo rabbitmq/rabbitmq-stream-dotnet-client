0560c0a2ac206d09c51ea957581158d47fe92003
Luke Bakken <luke@bakken.io>
2022-05-09 16:07:01 -0700

    Add TODO

--- RabbitMQ.Stream.Client/Consts.cs
+++ RabbitMQ.Stream.Client/Consts.cs
@@ -6,6 +6,7 @@ namespace RabbitMQ.Stream.Client
 {
     public static class Consts
     {
+        // TODO get this via reflection
         public const string ClientVersion = "1.0.0-beta.5";
     }
 }
