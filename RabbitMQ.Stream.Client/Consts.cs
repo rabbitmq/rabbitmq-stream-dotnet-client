604f321b814f06ec54d0c6af34f15afbedf01f72
Gabriele Santomaggio <G.santomaggio@gmail.com>
2022-05-12 01:42:57 +0200

    Add producer MessagesBufferSize (#56)
    * Add producer batch size
    
    Add tests and validation for stream name and bacth size limits
    
    remove max batch size
    
    rename property
    
    * Fixes after rebase
    
    * Fix formatting
    
    Co-authored-by: Luke Bakken <luke@bakken.io>

---
+++ RabbitMQ.Stream.Client/Consts.cs
@@ -0,0 +1,12 @@
+﻿// This source code is dual-licensed under the Apache License, version
+// 2.0, and the Mozilla Public License, version 2.0.
+// Copyright (c) 2007-2020 VMware, Inc.
+
+namespace RabbitMQ.Stream.Client
+{
+    internal static class Consts
+    {
+        internal const int MaxBatchSize = 10000;
+        internal const int MinBatchSize = 1;
+    }
+}
