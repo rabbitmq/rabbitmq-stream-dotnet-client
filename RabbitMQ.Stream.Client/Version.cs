4571ba142781049ad84157e5164294080c543c7c
Luke Bakken <luke@bakken.io>
2022-05-12 00:01:12 -0700

    Get version via reflection (#119)
    This removes a build step that would result in a modified file every time.

--- RabbitMQ.Stream.Client/Version.cs
+++ RabbitMQ.Stream.Client/Version.cs
@@ -2,10 +2,22 @@
 // 2.0, and the Mozilla Public License, version 2.0.
 // Copyright (c) 2007-2020 VMware, Inc.
 
+using System.Diagnostics;
+using System.Reflection;
+
 namespace RabbitMQ.Stream.Client
 {
     public static class Version
     {
-        public static readonly string VersionString = "1.0.0-beta.5.8";
+        private static readonly string _versionString;
+
+        static Version()
+        {
+            var a = Assembly.GetAssembly(typeof(Version));
+            var fvi = FileVersionInfo.GetVersionInfo(a.Location);
+            _versionString = fvi.ProductVersion;
+        }
+
+        public static string VersionString => _versionString;
     }
 }
