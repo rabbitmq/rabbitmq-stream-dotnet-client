99c39b24570d7bfce5d0dbc093740e3835541c3f
Gabriele Santomaggio <G.santomaggio@gmail.com>
2022-06-21 10:30:33 +0200

    Format
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- Tests/ReliableTests.cs
+++ Tests/ReliableTests.cs
@@ -1,4 +1,4 @@
-// This source code is dual-licensed under the Apache License, version
+﻿// This source code is dual-licensed under the Apache License, version
 // 2.0, and the Mozilla Public License, version 2.0.
 // Copyright (c) 2007-2020 VMware, Inc.
 
