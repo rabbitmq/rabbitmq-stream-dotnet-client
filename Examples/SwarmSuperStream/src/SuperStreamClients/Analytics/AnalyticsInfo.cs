2895022034d7c2e5f5966791ed4f86ca60d0197e
Ross Buggins <78215796+RossBugginsNHS@users.noreply.github.com>
2022-12-21 14:57:00 +0000

    Moved HostMessages to own file.

--- Examples/SwarmSuperStream/src/SuperStreamClients/Analytics/AnalyticsInfo.cs
+++ Examples/SwarmSuperStream/src/SuperStreamClients/Analytics/AnalyticsInfo.cs
@@ -5,11 +5,3 @@ public class AnalyticsInfo
     public List<string> Sources = new List<string>();
     public Dictionary<string, HostMessages > Hosts = new Dictionary<string, HostMessages>();
 }
-
-public class HostMessages
-{
-    public string Host{get;set;}
-    public TimeOnly TimeSeen{get;set;}
-    public  List<ReceivedCustomerMessage> Messages {get;set;} = new List<ReceivedCustomerMessage>(); 
-
-}
