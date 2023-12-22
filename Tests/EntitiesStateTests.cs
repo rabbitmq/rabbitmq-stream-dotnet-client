8db82332a67d99dc8ae8f9c8f339485082f47799
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-12-22 16:54:13 +0100

    Update license to Broadcom
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- Tests/EntitiesStateTests.cs
+++ Tests/EntitiesStateTests.cs
@@ -1,6 +1,6 @@
 ﻿// This source code is dual-licensed under the Apache License, version
 // 2.0, and the Mozilla Public License, version 2.0.
-// Copyright (c) 2017-2023 Broadcom. All Rights Reserved. The term Broadcom refers to Broadcom Inc. and/or its subsidiaries.
+// Copyright (c) 2007-2023 VMware, Inc.
 
 using System.Collections.Generic;
 using System.Text;
