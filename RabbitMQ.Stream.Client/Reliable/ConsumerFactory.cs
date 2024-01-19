9130e388820926e2906339111d15af17a6436c94
Gabriele Santomaggio <G.santomaggio@gmail.com>
2024-01-19 09:34:23 +0100

    formatting
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- RabbitMQ.Stream.Client/Reliable/ConsumerFactory.cs
+++ RabbitMQ.Stream.Client/Reliable/ConsumerFactory.cs
@@ -2,7 +2,6 @@
 // 2.0, and the Mozilla Public License, version 2.0.
 // Copyright (c) 2017-2023 Broadcom. All Rights Reserved. The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
 
-using System;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
