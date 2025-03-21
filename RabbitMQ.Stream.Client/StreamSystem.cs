f7237eb917af51aa0e0069b5199df4e394dbce2a
Falk Jonas - HK <Jonas.Falk@kriminalvarden.se>
2025-03-21 10:09:12 +0100

    Added hte ability to store offsets using the streamsystem, which can be used when you're not in the context of a messagehandler

--- RabbitMQ.Stream.Client/StreamSystem.cs
+++ RabbitMQ.Stream.Client/StreamSystem.cs
@@ -184,6 +184,11 @@ namespace RabbitMQ.Stream.Client
             }
         }
 
+        public async Task StoreOffset(string reference, string stream, ulong offsetValue)
+        {
+            await _client.StoreOffset(reference, stream, offsetValue).ConfigureAwait(false);
+        }
+
         public async Task UpdateSecret(string newSecret)
         {
             // store the old password just in case it will fail to update the secret
