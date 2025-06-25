1c4d253a98a17b42c3e668cabeec21733b2c6c08
Gabriele Santomaggio <G.santomaggio@gmail.com>
2025-06-25 19:35:32 +0200

    small doc fix
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- RabbitMQ.Stream.Client/StreamCrc32.cs
+++ RabbitMQ.Stream.Client/StreamCrc32.cs
@@ -18,6 +18,6 @@ public class StreamCrc32 : ICrc32
     {
         return System.IO.Hashing.Crc32.Hash(data);
     }
-    
+
     public Func<IConsumer, ChunkAction> FailAction { get; set; } = null;
 }
